fdd053260282752ef7839d07ed40f57b5976e707
stephan-caylent <117462968+stephan-caylent@users.noreply.github.com>
2024-03-21 17:30:53 -0500

    localhost

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ async def delete_todo(id: int):
     return {f"Product '{product.title}' deleted"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
